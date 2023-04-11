90ceefa89777a491e88444309eafe66d089e1b76
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-04-11 18:10:25 -0300

    adicionado lógica para ao clicar identificar o elemento dentro do fakeData

--- blog/src/app/data/dataFake.ts
+++ blog/src/app/data/dataFake.ts
@@ -1,14 +1,15 @@
 export const dataFake = [
   {
-    "id": "1",
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO ANUNCIADO',
+    description: 'Marvel studios anuncia novo filme do homem de ferro.',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsPRyS1u8OFeeDz1vsjLBycQJuHJ6WMKQuxQ&usqp=CAU',
   },
   {
-    "id": "2",
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    id: '2',
+    title: 'Saiu a nova versão do Angular',
+    description: 'bla bla bla',
+    photoCover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhvFqAAKNE1J9nrem3Vfyccscww9WjlCQGJg&usqp=CAU',
   },
-]
+];
